79c4cabb386d0ad7a2e9fc4b8f79aa0a2176598f
Josh Millgate <38218081+joshmillgate@users.noreply.github.com>
2021-10-19 11:26:36 +0100

    Update superdocs.js

--- superdocs.js
+++ superdocs.js
@@ -1,3 +1,9 @@
 $(document).ready(function () {
    $(".notion-header").prependTo(".notion-root > .notion-column-list > .notion-column:nth-child(2)")
 })
+document.addEventListener("DOMContentLoaded", function() {
+  const header = document.querySelector('.notion-header');
+  const content = document.querySelector('.notion-root > .notion-column-list > .notion-column:nth-child(2)');
+  
+  content.prepend(header, content.firstChild);
+});

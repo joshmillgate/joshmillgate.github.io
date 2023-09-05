e527c4d1c169bfc86d252aebbce39c2006697499
Josh Millgate <hello@joshmillgate.co.uk>
2023-09-05 14:48:34 +0100

    remove old

--- cluster/script-v2.js
+++
@@ -1,8 +0,0 @@
-if (!document.getElementById('nav-toggle')) {
-    if (document.querySelector('.notion-navbar')) {
-      document.querySelector('.notion-navbar').insertAdjacentHTML('beforeend', '<div id="nav-toggle" class="js-menu menu"><span class="bar"></span></div>');
-    }
-    if (document.querySelector('.super-navbar')) {
-      document.querySelector('.super-navbar__actions ').insertAdjacentHTML('beforeend', '<div id="nav-toggle" class="js-menu menu"><span class="bar"></span></div>');
-    }
-  }
